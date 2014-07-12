0c676a7e7ab43b8b37baf606eda768c58622d438
dxk <dxk@you-may.cn>
2014-07-12 09:59:37 +0800

    修改新闻加载的方式

---
+++ News/newJson.js
@@ -0,0 +1,6 @@
+$(function () {
+
+
+    {"Prid":21,"Prname":"·¢²Ë¿Ú","Prip":"192.168.1.177","Prtype":"58","Prqie":1,"Prjiao":0,"Prshan":1,"Prcopy":1,"Pr01":"¹ðÁÖÑç","Pr02":"0716-6666666","Hotid":0}
+
+});
